3af81d80c275d4f9fe7487a353bfa93e2e606e65
Vishwadeep Singh <vsdpsingh@gmail.com>
2026-09-12 02:00:56 +0530

    Add Jenkins CI pipeline

---
+++ cicd/install-jenkins.sh
@@ -0,0 +1,12 @@
+sudo apt update
+sudo apt install -y openjdk-21-jdk
+java -version
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc   https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update
+sudo apt install -y jenkins
+sudo systemctl status jenkins --no-pager
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+echo "open localhost:8080/"
+echo "register new user -> admin/password"
+
